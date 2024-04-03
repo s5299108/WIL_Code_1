01e0adac2084082189444454d43e225dab553501
tmagn5 <140672938+tmagn5@users.noreply.github.com>
2024-04-03 23:14:07 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print("Uif rvjdl tmjdl gpy kvnqfe pwfs uif cspxo mbaz eph")
+
+a = input("Enter a sentence: ")
+
+if a == "The quick slick fox jumped over the brown lazy dog":
+    print("CORRECT")
+else:
+    print("WRONG")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+# cap_a = a[0:].upper()
+# print(cap_a)
